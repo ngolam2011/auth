bfc9c24df210d8d041296eb724e6fb192023da38
Ngo Lam <lngo9@csc.com>
2018-03-08 18:32:34 +0700

    080

---
+++ config.js
@@ -0,0 +1,6 @@
+// Hold app secrets and config
+
+module.exports = {
+  secret: 'lafkjsdfsdfasdfete34234dfsd'
+};
+
---
+++ controllers/authentication.js
@@ -0,0 +1,39 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  // sub -> subject: who is token is belong to.
+  // iat -> isue at time
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+  // See if a user with the given email exists
+    if (existingUser) {
+      // If a user with email does exist, return an error
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User({ 
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+      // Respond to request indicating the user was created
+      res.json({ token: tokenForUser(user) });
+    });
+  });
+  
+}
