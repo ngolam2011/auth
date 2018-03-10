be0d49b469a7a30fb453762907f87d1f8e4ee37e
ngo lam <ngolam>
2018-03-10 17:25:16 +0700

    dfkj

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
-    res.send({ hi: 'there' });
+    res.send({ message: 'super secret code in ABC123' });
   });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
